203be8b35af737d55880ab4ba6fe8b6b02e9bb8c
Hunter Webb <44552711+RedH11@users.noreply.github.com>
2020-10-29 12:20:44 -0400

    - Added dynamic file pathing for the cvd training set - Created the image augmentation test for tensorflow with the catsvdogs dataset

--- Course 2: Convolutional Networks/augmentation_test.py
+++ Course 2: Convolutional Networks/augmentation_test.py
@@ -4,15 +4,17 @@ from tensorflow.keras.preprocessing.image import *
 from tensorflow.keras.optimizers import *
 from tensorflow.keras.callbacks import *
 import tensorflow as tf
-import os
-import time
-"""
-Bug Note: (For save best only)
-Setting it to monitor accuracy somehow worked instead of val_accuracy and the accuracy in the metrics also
-had to be 'accuracy' instead of 'acc'
 
-It also seems like adding the es_cb breaks it for some reason
 """
+Author: Hunter Webb
+Date: 10-25-20
+Summary:
+
+Using the cats vs dogs dataset as well as augmentation to expand the diveristy of the images to improve training
+even when using smaller amounts of data
+
+"""
+
 
 physical_devices = tf.config.list_physical_devices('GPU')
 try:
@@ -21,18 +23,20 @@ except:
   # Invalid device or cannot modify virtual devices once initialized.
   pass
 
-root_logdir = os.path.join(os.curdir, "../my_logs")
-
-def get_run_logdir():
-    run_id = time.strftime("run_%Y_%m_%d_%H_%M_%S")
-    return os.path.join(root_logdir, run_id)
-
-run_logdir = get_run_logdir()
-
 # Retrieving Data
-train_dir = os.getcwd() + '/cvd_train'
-print(train_dir)
-train_datagen = ImageDataGenerator(rescale=1./255, validation_split=0.1)
+train_dir = '/Users/hunterwebb/Documents/GitHub/hands_on_ml/Course 2: Convolutional Networks/cvd_train'
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    validation_split=0.1,
+    # Augmentation Settings
+    rotation_range=40, # How many degrees image is randomly rotated
+    width_shift_range=0.2, # Proportionate to image size, how much to move the subject horiz.
+    height_shift_range=0.2, # Proportionate to image size, how much to move the subject vert.
+    shear_range=0.2, # Skew the image along the x axis (in this case 20%) to make varied poses
+    zoom_range=0.2, # Zooms will be random up to 20%
+    horizontal_flip=True, # Images will be flipped horizontally at random
+    fill_mode='nearest' # Fill mode specifies what strategy to use to fill in the pixels that are lost in transformations
+)
 
 train_generator = train_datagen.flow_from_directory(
     train_dir,
@@ -40,6 +44,7 @@ train_generator = train_datagen.flow_from_directory(
     batch_size=20,
     class_mode='binary'
 )
+
 
 # Building Model
 model = Sequential([
@@ -65,7 +70,6 @@ model.compile(
 # Define Callbacks
 es_cb = EarlyStopping(patience=3, monitor='loss', restore_best_weights="True")
 checkpoint_cb = ModelCheckpoint('cvd_model.h5', monitor='accuracy', save_best_only=True)
-tensorboard_cb = TensorBoard(run_logdir)
 
 class AccuracyCallback(Callback):
     def on_epoch_end(self, epoch, logs={}):
@@ -82,5 +86,5 @@ history = model.fit(
     steps_per_epoch=100,
     validation_steps=50,
     shuffle=True,
-    callbacks=[acc_cb, checkpoint_cb, tensorboard_cb]
+    callbacks=[acc_cb, checkpoint_cb]
 )
