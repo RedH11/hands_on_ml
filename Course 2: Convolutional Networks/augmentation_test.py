aef7a65450d4b1975f78e7edbdb1982e4c8368d6
Hunter Webb <44552711+RedH11@users.noreply.github.com>
2020-10-29 12:29:17 -0400

    - Lowering batch size from 20 to 10 to not exceed memory limit

--- Course 2: Convolutional Networks/augmentation_test.py
+++ Course 2: Convolutional Networks/augmentation_test.py
@@ -41,7 +41,7 @@ train_datagen = ImageDataGenerator(
 train_generator = train_datagen.flow_from_directory(
     train_dir,
     target_size=(150, 150),
-    batch_size=20,
+    batch_size=10,
     class_mode='binary'
 )
 
--- Course 2: Convolutional Networks/catsvsdogs.py
+++ Course 2: Convolutional Networks/catsvsdogs.py
@@ -37,7 +37,7 @@ train_datagen = ImageDataGenerator(rescale=1./255, validation_split=0.1)
 train_generator = train_datagen.flow_from_directory(
     train_dir,
     target_size=(150, 150),
-    batch_size=20,
+    batch_size=10,
     class_mode='binary'
 )
 
